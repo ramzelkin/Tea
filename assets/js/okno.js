efc82cf7026fcb790075367a007ad2cc068194ed
ramzelkin <ramzelkin@gmail.com>
2017-06-14 23:05:36 +0300

    сделала проверку на ввод почты и сообщения

--- assets/js/okno.js
+++ assets/js/okno.js
@@ -1,5 +1,23 @@
 $(document).ready(function() { // вся магия после загрузки страницы
+document.getElementById('modal_form').onsubmit = function() {
+		var email = document.getElementById('mail').value;
+		var pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		var msg = document.getElementById('msg').value;
+		var result = pattern.test(email);
+		if (!result) {	
+		alert('Неправильный адрес');	
+		}
+		
+		 if (msg.length <= 0) {
+		 alert('Пустое сообщение');
+		 return false
+		 }
+		return result
+		
+	}
+
 	$('a#go').click( function(event){ // ловим клик по ссылки с id="go"
+		
 		event.preventDefault(); // выключаем стандартную роль элемента
 		$('#overlay').fadeIn(400, // сначала плавно показываем темную подложку
 		 	function(){ // после выполнения предъидущей анимации
